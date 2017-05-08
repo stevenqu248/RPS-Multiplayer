7f71ddcc60a53bd2ee9822c0388a525e29af16a6
Steven Quast <smquast@gmail.com>
2017-05-07 20:57:08 -0500

    Set up the game portion Set up the canned chat feature Set up system messages
    Next commit will be styling the webpage and cleaning up any extraneous stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,120 @@
-function initialize()
+var gameInitialized = false;
+var chatOptions = ["Hello", "Nice move!", "Good Game", "Ha! You suck!", "I won!", "Drat, a tie!", "Oh no, I lost!", "I'll get you next time!",
+					"You've stumbled right into my trap."];
+
+function initializeGame()
 {
-	
+	// add the score
+	if($("#score").is(":empty"))
+	{
+		$("#score").text("Wins: 0 | Losses: 0");
+	}
+
+	// add the game buttons
+	var gameArena = $("#game-arena");
+	var rockButton = $("<button>");
+	var paperButton = $("<button>");
+	var scissorsButton = $("<button>");
+	rockButton.attr("id","rock");
+	rockButton.attr("type","submit");
+	rockButton.text("Rock");
+	paperButton.attr("id","paper");
+	paperButton.attr("type","submit");
+	paperButton.text("Paper");
+	scissorsButton.attr("id","scissors");
+	scissorsButton.attr("type","submit");
+	scissorsButton.text("Scissors");
+	gameArena.append(rockButton);
+	gameArena.append(paperButton);
+	gameArena.append(scissorsButton);
+
+	// add the chat options
+	for(var i = 0; i < chatOptions.length; i++)
+	{
+		var newButton = $("<button>");
+		newButton.attr("class", "chat-btn");
+		newButton.text(chatOptions[i]);
+		$("#canned-options").append(newButton);
+	}
+
+
+	gameInitialized = true;
+	console.log("initialized");
+}
+
+function shutdownGame()
+{
+	// clean up everything
+	$("#game-arena").empty();
+	$("#name").empty();
+	$("#player-chat").empty();
+	$("#opponent-chat").empty();
+	$("#score").empty();
+	$("system-message").empty();
+	$("canned-options").empty();
+	gameInitialized = false;
+	console.log("shutdown");
+}
+
+function checkForWin(User1Choice, User2Choice)
+{
+	// Making the check quicker
+	switch(User1Choice)
+	{
+		case "ROCK":
+		{
+			User1Choice = 0;
+			break;
+		}
+		case "PAPER":
+		{
+			User1Choice = 1;
+			break;
+		}
+		case "SCISSORS":
+		{
+			User1Choice = 2;
+			break;
+		}
+	}
+
+	switch(User2Choice)
+	{
+		case "ROCK":
+		{
+			User2Choice = 0;
+			break;
+		}
+		case "PAPER":
+		{
+			User2Choice = 1;
+			break;
+		}
+		case "SCISSORS":
+		{
+			User2Choice = 2;
+			break;
+		}
+	}
+
+	var infoDiv = $("#system-message");
+	// And now for the actual check
+	if ((User2Choice + 1) % 3  == User1Choice)
+	{
+		// Player 1 wins
+		infoDiv.text("Player 1 wins!");
+    	return "User1";
+	}
+	else if ((User1Choice + 1) % 3 == User2Choice)
+	{
+		// Player 2 wins
+		infoDiv.text("Player 2 wins!");
+    	return "User2"
+	}
+	else
+	{
+		// it's a draw
+		infoDiv.text("It's a draw!");
+    	return "Neither"
+	}
 }
--- assets/javascript/network.js
+++ assets/javascript/network.js
@@ -10,31 +10,95 @@ var config =
 firebase.initializeApp(config);
 
 var database = firebase.database();
-var User1;
-var User2;
+// var messager = firebase.messaging();
+var User1, User2, slot1Ref, slot2Ref, User1Choice, User2Choice, choiceRef, playerChat, opponentChat, chatTimer;
 var playerPosition = null;
-var slot1Ref, slot2Ref;
+var winCount = 0;
+var lossCount = 0;
 
-// a listener for value changes for the User1 value in the database
-database.ref("User1").on("value",function(snapshot)
+// a listener for value changes in the database
+database.ref().on("value", function(snapshot)
 {
-	// check if there is data there
-	if(snapshot.val() != null)
-	{
-		console.log(snapshot.val());
-		User1 = snapshot.val();
-	}
-});
+	console.log(snapshot.val());
+	User1 = snapshot.val().User1;
+	User2 = snapshot.val().User2;
+	User1Choice = snapshot.val().User1Choice;
+	User2Choice = snapshot.val().User2Choice;
 
-// a listener for value changes for the User2 value in the database
-database.ref("User2").on("value",function(snapshot)
-{
-	// check if there is data there
-	if(snapshot.val() != null)
+	// Get the chat data
+	if(playerPosition == "User1")
 	{
-		console.log(snapshot.val());
-		User2 = snapshot.val();
+		playerChat = snapshot.val().User1Chat;
+		opponentChat = snapshot.val().User2Chat;
 	}
+
+	// Get the chat data
+	else if(playerPosition == "User2")
+	{
+		playerChat = snapshot.val().User2Chat;
+		opponentChat = snapshot.val().User1Chat;
+	}
+
+	// And put the chat on the screen
+	if(playerPosition != null)
+	{
+		$("#player-chat").text("Player's message: " + ((playerChat != "NONE")? playerChat : ""));
+		$("#opponent-chat").text("Opponent's message: " + ((opponentChat != "NONE")? opponentChat : ""));
+	}
+
+	// if both User1 and User2 are connected
+	if(User1 == "CONNECTED" && User2 == "CONNECTED" && playerPosition != null)
+	{
+		// and the game isn't created yet
+		if(!gameInitialized)
+		{
+			initializeGame();
+			var playerName = $("#name");
+			playerName.append((playerPosition == "User1")? "Player 1": "Player 2");
+
+			// clean up the screen
+			$("#system-message").empty();
+		}
+	}
+
+	// otherwise
+	else if (User1 == "VACANT" || User2 == "VACANT")
+	{
+		// if the game is running
+		if(gameInitialized)
+		{
+			$("#name").empty();
+			shutdownGame();
+			playerPosition = null;
+		}
+	}
+
+	if(User1Choice != "NONE" && User2Choice != "NONE" && playerPosition != null)
+	{
+		console.log(User1Choice + " " + User2Choice);
+		var winner = checkForWin(User1Choice, User2Choice);
+		User1Choice = User2Choice = "NONE";
+
+		// I don't need to do a listener because I'm setting it to something I want even if they disconnect
+		choiceRef = database.ref(playerPosition + "Choice");
+		choiceRef.set("NONE");
+		
+		// if the player won
+		if(playerPosition == winner)
+		{
+			winCount++;
+		}
+
+		// if the player lost
+		else if(playerPosition != winner && winner != "Neither")
+		{
+			lossCount++;
+		}
+
+		// and update the scoreboard
+		$("#score").text("Wins: " + winCount + " | Losses: " + lossCount);
+	}
+
 });
 
 // a listener for connecting to the game
@@ -45,6 +109,10 @@ $(document).on("click", "#connect-btn",function(event)
 	// if not already connected
 	if(playerPosition == null)
 	{
+		// clean up any information that may be left over from other things
+		var infoDiv = $("#system-message");
+		infoDiv.empty();
+
 		// if spot 1 is open
 		if(User1 == "VACANT")
 		{
@@ -53,9 +121,13 @@ $(document).on("click", "#connect-btn",function(event)
 			slot1Ref.onDisconnect().set("VACANT");
 
 			// then modify server data
-			slot1Ref.set("CONNECTED");
 			playerPosition = "User1";
+			slot1Ref.set("CONNECTED");
 			console.log(playerPosition);
+
+			// inform the player of success
+			if(User2 == "VACANT")
+				infoDiv.append("Successfully connected to the game. Please wait for another player to join.");
 		}
 
 		// else if spot 2 is open
@@ -66,15 +138,19 @@ $(document).on("click", "#connect-btn",function(event)
 			slot2Ref.onDisconnect().set("VACANT");
 
 			// then modify server data
+			playerPosition = "User2";
 			slot2Ref.set("CONNECTED");
-			playerPosition = "User1";
 			console.log(playerPosition);
+
+			// inform the player of success
+			if(User1 == "VACANT")
+				infoDiv.append("Successfully connected to the game. Please wait for another player to join.");
 		}
 
 		// let the user know that all available slots are full
 		else
 		{
-
+			infoDiv.append("I'm sorry. All available game slots are full. Please try again at a later time.");
 		}
 	}
 });
@@ -83,10 +159,95 @@ $(document).on("click", "#connect-btn",function(event)
 $(document).on("click", "#disconnect-btn", function(event)
 {
 	event.preventDefault();
-
+	console.log(playerPosition);
 	if(playerPosition != null)
 	{
 		var slotRef = database.ref(playerPosition);
 		slotRef.set("VACANT");
+		playerPosition = null;
 	}
+});
+
+$(document).on("click", "#rock", function(event)
+{
+	event.preventDefault();
+	if(playerPosition == null)
+		alert("You Shouldn't be here!");
+	
+	else
+	{
+		if(playerPosition == "User1" && User1Choice == "NONE" || playerPosition == "User2" && User2Choice == "NONE")
+		{
+			// set up a listener for if the user disconnects
+			choiceRef = database.ref(playerPosition + "Choice");
+			choiceRef.onDisconnect().set("NONE");
+		
+			// then modify server data
+			choiceRef.set("ROCK");
+		}
+	}
+});
+
+$(document).on("click", "#paper", function(event)
+{
+	event.preventDefault();
+	if(playerPosition == null)
+		alert("You Shouldn't be here!");
+
+	else
+	{
+		//  if the player is player 1 and they haven't chosen or if the player is player 2 and they haven't chosen
+		if(playerPosition == "User1" && User1Choice == "NONE" || playerPosition == "User2" && User2Choice == "NONE")
+		{
+			// set up a listener for if the user disconnects
+			choiceRef = database.ref(playerPosition + "Choice");
+			choiceRef.onDisconnect().set("NONE");
+
+			// then modify server data
+			choiceRef.set("PAPER");	
+		}
+	}
+});
+
+$(document).on("click", "#scissors", function(event)
+{
+	event.preventDefault();
+	if(playerPosition == null)
+		alert("You Shouldn't be here!");
+
+	else
+	{
+		if(playerPosition == "User1" && User1Choice == "NONE" || playerPosition == "User2" && User2Choice == "NONE")
+		{
+			// set up a listener for if the user disconnects
+			choiceRef = database.ref(playerPosition + "Choice");
+			choiceRef.onDisconnect().set("NONE");
+
+			// then modify server data
+			choiceRef.set("SCISSORS");	
+		}	
+	}
+});
+
+function clearText()
+{
+	choiceRef = database.ref(playerPosition + "Chat");
+	choiceRef.set("NONE");
+}
+
+$(document).on("click", ".chat-btn", function(event)
+{
+	event.preventDefault();
+
+	console.log(event.target.textContent);
+	// stop any other timers from clearing new incoming text too early
+	clearTimeout(chatTimer);
+
+	// set up a listener for if the user disconnects
+	choiceRef = database.ref(playerPosition + "Chat");
+	choiceRef.onDisconnect().set("NONE");
+
+	// then modify server data
+	choiceRef.set(event.target.textContent);
+	chatTimer = setTimeout(clearText, 5000);
 });
